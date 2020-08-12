abdd1f382e94d0e9587b19a8a9cec8fdb49faa5c
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-08-12 17:33:23 -0600

    done

--- init.sql
+++ init.sql
@@ -1,4 +1,7 @@
--- prob 1
+--Join Pactice
+
+
+--  prob 1
 
 SELECT * FROM invoice 
 JOIN invoice_line  ON invoice_line.invoice_id = invoice.invoice_id
@@ -45,3 +48,87 @@ SELECT track.name, album.title FROM track
 JOIN album  ON track.album_id = album.album_id
 JOIN genre  ON genre.genre_id = track.genre_id
 WHERE genre.name = 'Alternative and Punk';
+
+
+
+
+
+-- PRACTICE NESTED QUERIES
+
+-- prob 1
+
+SELECT * FROM invoice
+WHERE invoice_id IN 
+                ( SELECT invoice_id FROM invoice_line
+                WHERE unit_price > .99)
+
+-- prob 2
+
+SELECT * FROM playlist_track
+WHERE playlist_id IN
+        ( SELECT playlist_id FROM playlist
+        WHERE name = 'Music' )
+
+-- prob 3
+
+SELECT * FROM track
+where track_id IN
+        ( SELECT track_id FROM playlist_track
+        WHERE playlist_id = 5 )
+
+-- prob 4
+
+SELECT * FROM track
+WHERE genre_id IN 
+        ( SELECT genre_id FROM genre
+        WHERE name = 'Comedy' )
+
+-- prob 5
+
+SELECT * FROM track
+WHERE album_id IN
+        ( SELECT album_id FROM album
+        WHERE title = 'Fireball' )
+
+
+
+
+
+-- practice updating ROWS
+
+
+-- prob 1
+
+UPDATE customer
+SET fax = NULL
+
+SELECT * FROM customer
+
+-- prob 2
+
+UPDATE customer
+SET company = 'self'
+WHERE company IS NULL
+
+-- prob 3
+
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE last_name = 'Barnett'
+
+SELECT * FROM customer
+where last_name = 'Thompson'
+
+-- prob 4
+
+UPDATE customer
+SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl'
+
+-- prob 5
+
+
+
+
+
+
